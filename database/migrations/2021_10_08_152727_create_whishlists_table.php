66b4456b50dd3aa8e2c409013163dbf08e892b67
unknown <sergeysoftcode@gmail.com>
2021-10-08 19:29:05 +0400

    add Products for Admin and for front done , now working in wishlist -40%

--- database/migrations/2021_10_08_152727_create_whishlists_table.php
+++ database/migrations/2021_10_08_152727_create_whishlists_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateWishlistsTable extends Migration
+class CreateWhishlistsTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,7 +13,7 @@ class CreateWishlistsTable extends Migration
      */
     public function up()
     {
-        Schema::create('wishlists', function (Blueprint $table) {
+        Schema::create('whishlists', function (Blueprint $table) {
             $table->id();
             $table->integer('user_id');
             $table->integer('product_id');
@@ -32,6 +32,6 @@ class CreateWishlistsTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('wishlists');
+        Schema::dropIfExists('whishlists');
     }
 }
